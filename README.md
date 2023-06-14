1358e4509cbf5b24a1e6dc8970880ca109e8ef94
BohdanObruch <bodan1992@gmail.com>
2023-06-14 23:16:57 +0300

    Added a project description to the Readme. Added code to create a chatbot for learning English

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# EnglishTelegramBot
-Telegram bot for learning English words
+# English Telegram Bot
+An example of developing a telegram bot from scratch using APIs: unsplash, deepl, rapidapi , voicerss and telegram
+
+* Unsplash - https://unsplash.com/
+* DeepL Translator - https://www.deepl.com/
+* Rapid api - https://rapidapi.com/rokish/api/lingua-robot
+* Voice RSS - https://www.voicerss.org/
+<br>
+
+<br>
+
+* ✅ The bot uses the Lingua Robot library with 800 thousand English words. 
+* ✅ Words from the library are extracted in random order and are not repeated if the word has already been added to the database as a word to repeat after learning. 
+* ✅ Each word is accompanied by a picture that shows the meaning of the word thanks to the unsplash library. 
+* ✅ For each word, a transcription is displayed, which is also available in the Lingua Robot library for each word. 
+* ✅ Using to the DEEPL library, the translation of words into Ukrainian is displayed. 
+* ✅ Using to the voicerss api, I display the voice of the word that is being learned or repeated after learning.
+* ✅ Words can be memorized (for later repetition) or skipped.
+* ✅ The database contains the words being studied, their transcription, a link to a picture, definition, and the user's chat_id.
+* ✅ Repetition of words occurs from words that have been previously saved.
+* ✅ The user can delete all the words he has learned from the database ( those that have been saved for repetition)
+* ✅ Only one image with text and audio is displayed and stored in the chat, when new words are displayed, the previous value is deleted from the chat display
+
+<br>
+
+<br>
+
+> This is what the bot will look like. 
+<p align="center">
+<img src="resources/images/example.JPG" alt=""/>
+</p>
+<br>
+
+> An example of how saved words are displayed in a sqlite database.
+<p align="center">
+<img src="resources/images/example2.JPG" alt="database"/>
+</p>
---
+++ bot.py
@@ -0,0 +1,271 @@
+import logging
+import requests
+import random
+import json
+import os
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler, MessageHandler, Filters
+import sqlite3
+from contextlib import closing
+
+TELEGRAM_TOKEN = "*****"
+UNSPLASH_ACCESS_KEY = "******"
+DEEPL_API_KEY = "*******"
+LINGUA_ROBOT_API_KEY = "******"
+VOICE_KEY = "*******"
+DATABASE = 'words.db'
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+conn = sqlite3.connect(DATABASE, check_same_thread=False)
+cursor = conn.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS words
+                  (chat_id text, word text, definition text, image_url text, translated_word text, transcription text)''')
+
+
+def translate_word(word):
+    url = "https://api-free.deepl.com/v2/translate"
+    headers = {
+        "Authorization": f"DeepL-Auth-Key {DEEPL_API_KEY}",
+    }
+    data = {
+        "text": word,
+        "target_lang": "UK",
+    }
+    response = requests.post(url, headers=headers, data=data)
+    response_json = json.loads(response.text)
+    translated_text = response_json["translations"][0]["text"]
+    return translated_text
+
+
+def get_random_word():
+    word = None
+    definition = None
+    transcription = None
+    while word is None or definition is None or transcription is None:
+        try:
+            url = "https://api.datamuse.com/words?ml=love"
+            response = requests.get(url).json()
+            random_word = random.choice(response)
+            word = random_word['word']
+
+            cursor.execute("SELECT * FROM words WHERE word=?", (word,))
+            if cursor.fetchone() is not None:
+                word = None
+                definition = None
+                transcription = None
+                continue
+
+            url = f"https://lingua-robot.p.rapidapi.com/language/v1/entries/en/{word}"
+            headers = {
+                "X-RapidAPI-Key": LINGUA_ROBOT_API_KEY,
+                "X-RapidAPI-Host": "lingua-robot.p.rapidapi.com"
+            }
+            response = requests.get(url, headers=headers).json()
+            definition = response['entries'][0]['lexemes'][0]['senses'][0]['definition']
+            transcription = response['entries'][0]['pronunciations'][0]['transcriptions'][0]['transcription']
+            transcription = transcription.replace('/', '')
+
+        except (KeyError, IndexError):
+            word = None
+            definition = None
+            transcription = None
+    return word, definition, transcription
+
+
+def get_image(word):
+    url = f"https://api.unsplash.com/search/photos?query={word}&client_id={UNSPLASH_ACCESS_KEY}"
+    response = requests.get(url).json()
+    image_url = response['results'][0]['urls']['small']
+    return image_url
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    keyboard = [
+        [KeyboardButton("📘 Learn"),
+         KeyboardButton("🔄 Repeat"),
+         KeyboardButton("🗑️ Delete the words")]
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Choose an option:", reply_markup=reply_markup)
+
+
+def handle_messages(update: Update, context: CallbackContext) -> None:
+    text = update.message.text
+    if text == "📘 Learn":
+        learn(update, context)
+    elif text == "🔄 Repeat":
+        review(update, context)
+    elif text == "🗑️ Delete the words":
+        clear(update, context)
+
+
+def learn(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+    loading_message = context.bot.send_message(chat_id=chat_id, text="Wait for the words...")
+    word, definition, transcription = get_random_word()
+    image_url = get_image(word)
+    translated_word = translate_word(word)
+    context.chat_data['word_data'] = (chat_id, word, definition, image_url, translated_word, transcription)
+
+    url = f"http://api.voicerss.org/?key={VOICE_KEY}&hl=en-us&v=Linda&src={word}&r=-2"
+    response = requests.request("GET", url)
+
+    valid_filename = "".join(c for c in word if c.isalpha() or c.isspace()).rstrip()
+
+    with open(f'{valid_filename}.mp3', 'wb') as audio_file:
+        audio_file.write(response.content)
+
+    keyboard = [
+        [InlineKeyboardButton("Remember", callback_data=f'remembered_{word}'),
+         InlineKeyboardButton("Skip", callback_data=f'skip_{word}')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    sent_message = context.bot.send_photo(chat_id=chat_id, photo=image_url,
+                                          caption=f"{word} [{transcription}] - {translated_word}\n\n{definition}",
+                                          reply_markup=reply_markup)
+
+    with closing(open(f'{valid_filename}.mp3', 'rb')) as audio_file:
+        audio_message = context.bot.send_audio(chat_id=chat_id, audio=audio_file)
+
+    context.bot.delete_message(chat_id=chat_id, message_id=loading_message.message_id)
+
+    # Save the IDs of the sent message and audio file to delete later
+    context.chat_data['last_message'] = sent_message.message_id
+    context.chat_data['last_audio'] = audio_message.message_id
+
+    os.remove(f"{valid_filename}.mp3")
+
+
+def clear(update: Update, context: CallbackContext) -> None:
+    query = update
+    chat_id = query.message.chat_id
+    cursor.execute("DELETE FROM words WHERE chat_id=?", (chat_id,))
+    conn.commit()
+    context.bot.send_message(chat_id=chat_id, text="All learned words are deleted.")
+    start(query, context)
+
+
+def button(update: Update, context: CallbackContext) -> None:
+    query = update.callback_query
+    query.answer()
+
+    # Delete the previous message and audio file before doing anything else
+    if 'last_message' in context.chat_data:
+        try:
+            context.bot.delete_message(chat_id=query.message.chat_id, message_id=context.chat_data['last_message'])
+            del context.chat_data['last_message']
+        except Exception as e:
+            print(f'Error deleting last message: {e}')
+    if 'last_audio' in context.chat_data:
+        try:
+            context.bot.delete_message(chat_id=query.message.chat_id, message_id=context.chat_data['last_audio'])
+            del context.chat_data['last_audio']
+        except Exception as e:
+            print(f'Error deleting last audio: {e}')
+
+    if query.data == 'learn':
+        learn(query, context)
+    elif query.data == 'review':
+        review(query, context)
+    elif query.data == 'clear':
+        clear(query, context)
+    elif query.data.startswith('remembered_'):
+        cursor.execute("INSERT INTO words VALUES (?, ?, ?, ?, ?, ?)", context.chat_data['word_data'])
+        conn.commit()
+        learn(query, context)
+    elif query.data.startswith('skip_'):
+        learn(query, context)
+    elif query.data.startswith('next_'):
+        review(query, context)
+    elif query.data == 'end_review':
+        context.bot.send_message(chat_id=query.message.chat_id, text="Repetition is over.")
+        context.chat_data['review_index'] = 0
+        start(update, context)
+
+
+def review(update: Update, context: CallbackContext) -> None:
+    chat_id = update.message.chat_id
+
+    # Delete the previous message and audio file before sending a new one
+    if 'last_message' in context.chat_data:
+        try:
+            context.bot.delete_message(chat_id=chat_id, message_id=context.chat_data['last_message'])
+            del context.chat_data['last_message']
+        except Exception as e:
+            print(f'Error deleting last message: {e}')
+    if 'last_audio' in context.chat_data:
+        try:
+            context.bot.delete_message(chat_id=chat_id, message_id=context.chat_data['last_audio'])
+            del context.chat_data['last_audio']
+        except Exception as e:
+            print(f'Error deleting last audio: {e}')
+
+    if 'review_index' not in context.chat_data or context.chat_data['review_index'] == 0:
+        context.chat_data['review_index'] = 0
+        word = cursor.execute("SELECT * FROM words WHERE chat_id=? LIMIT 1 OFFSET ?",
+                              (chat_id, context.chat_data['review_index'])).fetchone()
+    else:
+        word = cursor.execute("SELECT * FROM words WHERE chat_id=? LIMIT 1 OFFSET ?",
+                              (chat_id, context.chat_data['review_index'])).fetchone()
+
+    if word:
+        keyboard = [
+            [InlineKeyboardButton("Next", callback_data=f'next_{word[1]}'),
+             InlineKeyboardButton("End repetition", callback_data='end_review')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+
+        url = f"http://api.voicerss.org/?key={VOICE_KEY}&hl=en-us&v=Linda&src={word[1]}&r=-2"
+        response = requests.request("GET", url)
+
+        valid_filename = "".join(c for c in word[1] if c.isalpha() or c.isspace()).rstrip()
+
+        audio_filename = f'{valid_filename}.mp3'
+        with open(audio_filename, 'wb') as audio_file:
+            audio_file.write(response.content)
+
+        # Send image if exists
+        if word[3]:  # Assuming word[3] is the column where you store the image URL
+            sent_message = context.bot.send_photo(chat_id=chat_id, photo=word[3],
+                                                  caption=f'{word[1]} [{word[5]}] - {word[4]}\n{word[2]}',
+                                                  reply_markup=reply_markup)
+        else:
+            sent_message = context.bot.send_message(chat_id=chat_id,
+                                                    text=f'{word[1]} [{word[5]}] - {word[4]}\n{word[2]}',
+                                                    reply_markup=reply_markup)
+
+        with open(audio_filename, 'rb') as audio_file:
+            audio_message = context.bot.send_audio(chat_id=chat_id, audio=audio_file)
+
+        context.chat_data['review_index'] += 1
+
+        # Save the IDs of the sent message and audio file to delete later
+        context.chat_data['last_message'] = sent_message.message_id
+        context.chat_data['last_audio'] = audio_message.message_id
+
+        os.remove(audio_filename)
+    else:
+        context.bot.send_message(chat_id=chat_id, text="All learned words are repeated.")
+        context.chat_data['review_index'] = 0  # Reset the review index when all words have been reviewed
+
+
+def next_word(update: Update, context: CallbackContext) -> None:
+    # query = update.callback_query
+    review(update, context)
+
+
+def main() -> None:
+    updater = Updater(token=TELEGRAM_TOKEN)
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, handle_messages))
+    dispatcher.add_handler(CallbackQueryHandler(button))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
